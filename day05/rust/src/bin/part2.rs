b94d6d4ac1af17107f3d2b9090ced54f55e3c9a9
Jabba Laci <jabba.laci@gmail.com>
2019-12-06 06:26:56 +0100

    day 5, rust

---
+++ day05/rust/src/bin/part2.rs
@@ -0,0 +1,176 @@
+use day05::helper::read_input_file;
+
+const INPUT_FILE: &str = "input.txt";
+
+
+#[derive(Debug, PartialEq)]
+enum Mode {
+    PositionMode,
+    ImmediateMode
+}
+
+#[derive(Debug)]
+struct Instruction {
+    opcode: i32,
+    first_param_mode: Mode,
+    second_param_mode: Mode,
+    third_param_mode: Mode
+}
+
+fn read_input() -> i32 {
+    5
+}
+
+fn read_instruction(number: i32) -> Instruction
+{
+    fn to_mode(c: char) -> Mode {
+        if c == '0' {
+            Mode::PositionMode
+        } else {
+            Mode::ImmediateMode
+        }
+    }
+
+    if number == 99 {
+        // special case, it'll halt the program; parameter modes are not used, they get a dummy value
+        return Instruction {
+            opcode: number, first_param_mode: Mode::PositionMode,
+            second_param_mode: Mode::PositionMode, third_param_mode: Mode::PositionMode
+        }
+    }
+
+    let s = number.to_string();
+    let result = {
+        if s.len() == 1
+        {
+            let opcode = number;
+            let first_param_mode = Mode::PositionMode;
+            let second_param_mode = Mode::PositionMode;
+            let third_param_mode = Mode::PositionMode;
+            Instruction {    // yield
+                opcode: opcode, first_param_mode: first_param_mode,
+                second_param_mode: second_param_mode, third_param_mode: third_param_mode
+            }
+        }
+        else
+        {
+            let opcode: i32 = (&s[s.len()-2..]).replace("0", "").parse().unwrap();
+            let params: Vec<char> = format!("{:0>3}", &s[..s.len()-2]).chars().collect();
+            let first_param_mode = to_mode(params[params.len()-1]);
+            let second_param_mode = to_mode(params[params.len()-2]);
+            let third_param_mode = to_mode(params[params.len()-3]);
+            Instruction {    // yield
+                opcode: opcode, first_param_mode: first_param_mode,
+                second_param_mode: second_param_mode, third_param_mode: third_param_mode
+            }
+        }
+    };
+
+    result
+}
+
+fn run(program: &Vec<i32>) -> Vec<i32>
+{
+    fn get_param(data: &Vec<i32>, value: i32, mode: Mode) -> i32 {
+        if mode == Mode::ImmediateMode {
+            value
+        } else {
+            data[value as usize]
+        }
+    }
+
+    let mut data = program.to_vec();    // copy
+
+    let mut idx: usize = 0;
+    let mut inst = read_instruction(data[idx]);
+    while inst.opcode != 99
+    {
+        if inst.opcode == 1
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            let inp2 = get_param(&data, data[idx + 2], inst.second_param_mode);
+            let pos = data[idx + 3] as usize;
+            data[pos] = inp1 + inp2;
+            idx += 4;
+        }
+        else if inst.opcode == 2
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            let inp2 = get_param(&data, data[idx + 2], inst.second_param_mode);
+            let pos = data[idx + 3] as usize;
+            data[pos] = inp1 * inp2;
+            idx += 4;
+        }
+        else if inst.opcode == 3
+        {
+            let inp = read_input();
+            let pos = data[idx + 1] as usize;
+            data[pos] = inp;
+            idx += 2;
+        }
+        else if inst.opcode == 4
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            println!("Output: {}", inp1);
+            idx += 2;
+        }
+        else if inst.opcode == 5    // jump-if-true
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            let inp2 = get_param(&data, data[idx + 2], inst.second_param_mode);
+            if inp1 != 0 {
+                idx = inp2 as usize;
+            }
+            else {
+                idx += 3;
+            }
+        }
+        else if inst.opcode == 6    // jump-if-false
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            let inp2 = get_param(&data, data[idx + 2], inst.second_param_mode);
+            if inp1 == 0 {
+                idx = inp2 as usize;
+            }
+            else {
+                idx += 3;
+            }
+        }
+        else if inst.opcode == 7    // less than
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            let inp2 = get_param(&data, data[idx + 2], inst.second_param_mode);
+            let pos = data[idx + 3];
+            data[pos as usize] = {
+                if inp1 < inp2 { 1 } else { 0 }
+            };
+            idx += 4;
+        }
+        else if inst.opcode == 8    // equals
+        {
+            let inp1 = get_param(&data, data[idx + 1], inst.first_param_mode);
+            let inp2 = get_param(&data, data[idx + 2], inst.second_param_mode);
+            let pos = data[idx + 3];
+            data[pos as usize] = {
+                if inp1 == inp2 { 1 } else { 0 }
+            };
+            idx += 4;
+        }
+        else {
+            println!("Error: invalid opcode!");
+        }
+        inst = read_instruction(data[idx]);
+    }
+
+    data
+}
+
+fn main()
+{
+    let lines: Vec<String> = read_input_file(INPUT_FILE);
+    let line = &lines[0];
+
+    let program: Vec<i32> = line.split(',').map(|s| s.parse().unwrap()).collect();
+
+    run(&program);
+}
---
+++ day05/rust/src/helper.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn read_input_file(fname: &str) -> Vec<String>
+{
+    let file = File::open(fname).unwrap();
+    let reader = BufReader::new(file);
+    let mut lines = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        lines.push(line);
+    }
+    lines
+}
